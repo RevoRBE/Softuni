db830a8fc5deed4fbe9711a6897d1b1664179591
Krum Savov <krum.savov@gmail.com>
2018-03-07 14:34:46 +0200

    ADD Bashsoft

---
+++ BashSoft/BashSoft/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BashSoft
+{
+    class Launcher
+    {
+        static void Main()
+        {
+            //IOManager.TraverseDirectory(@"C:\Users\Revolutionist\Documents\Visual Studio 2017\Projects\BashSoft");
+            StudentsRepository.InitializeData();
+            StudentsRepository.GetAllStudentsFromCourse("C#_Jul_2016");
+        }
+    }
+}
